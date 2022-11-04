3cea2aed09eeb001603c9d7ae680b1dbd6b920b6
Aynh <99479536+Aynh@users.noreply.github.com>
2022-11-04 13:19:49 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#11253A",
+        body: "#333333",
+        footer: "#0C1A29"
+      },
+      fontFamily: {
+        display: "'Lexend Zetta', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
